a15b33f8c8b2b84e5fdb7c9970d19a18c1f76ea0
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-04-03 08:37:11 +0000

    feat: add expenses filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
-import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
+
+import { useState } from 'react';
+
+import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    
+    const filterChageHandler = year => {
+        setFilteredYear(year);
+    }
+
     const expenses = props.expenses;
     return (
         <Card className='expenses'>
+            <ExpensesFilter onChangeFilter={filterChageHandler} selected={filteredYear}/>
             <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
             <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
             <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
