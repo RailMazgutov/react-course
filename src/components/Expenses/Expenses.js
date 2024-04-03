7f6b6ed7f18d73c37ea35358b8d520e6b2e62ab2
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-04-03 09:01:53 +0000

    feat: add ids to list items

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,7 +17,7 @@ const Expenses = (props) => {
     return (
         <Card className='expenses'>
             <ExpensesFilter onChangeFilter={filterChageHandler} selected={filteredYear}/>
-            {expenses.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
+            {expenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
         </Card>
     )
 }
