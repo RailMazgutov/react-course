0d5444627d16b1034ba37d3cef79ad61a47bf349
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-04-03 08:51:01 +0000

    feat: render expenses dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,10 +17,7 @@ const Expenses = (props) => {
     return (
         <Card className='expenses'>
             <ExpensesFilter onChangeFilter={filterChageHandler} selected={filteredYear}/>
-            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+            {expenses.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
         </Card>
     )
 }
