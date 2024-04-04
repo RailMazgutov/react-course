0fca7eb9a966a9ca7eac7bf8ab214d3ce2262e13
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-04-04 21:24:31 +0200

    feat: refactor conditional output

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -16,20 +17,11 @@ const Expenses = (props) => {
     const expenses = props.expenses.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
-    let expensesContent = <p>No expenses.</p>
-    if (expenses.length > 0) {
-        expensesContent = expenses.map(expense =>
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}/>)
-    }
 
     return (
         <Card className='expenses'>
             <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
-            {expensesContent}
+            <ExpensesList items={expenses}/>
         </Card>
     )
 }
