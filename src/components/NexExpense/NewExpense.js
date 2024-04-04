1663697a59c3814b0210f86c5806037c9cba7d21
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-04-04 21:35:35 +0200

    feat: show form dynamically

--- src/components/NexExpense/NewExpense.js
+++ src/components/NexExpense/NewExpense.js
@@ -1,17 +1,34 @@
 import './NewExpense.css'
 import ExpenseForm from "./ExpenseForm";
+import {useState} from "react";
 
 const NewExpense = (props) => {
+    const [isEditing, setEditing] = useState(false);
     const expenseDataSavedHandler = expense => {
         const newExpense = {
             ...expense,
             id: Math.random().toString()
         }
+        setEditing(false);
         props.onExpenseAdded(newExpense)
     }
+
+    const expenseEditingCanceledHandler = () => {
+        setEditing(false);
+    };
+
+    const addExpenseHandler = () => {
+        setEditing(true);
+    };
+
     return (
         <div className='new-expense'>
-            <ExpenseForm onExpenseDataSaved={expenseDataSavedHandler}/>
+            {!isEditing && <button type="button" onClick={addExpenseHandler}>Add Expense</button>}
+            {isEditing &&
+                <ExpenseForm
+                    onCancel={expenseEditingCanceledHandler}
+                    onExpenseDataSaved={expenseDataSavedHandler}/>
+            }
         </div>
     );
 }
