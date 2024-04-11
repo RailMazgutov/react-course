93dd3c97fd6ef9594eae9832b601dc6836e079a2
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-04-11 10:18:47 +0200

    feat: handle events

--- investments/src/components/InestmentForm/InvestmentForm.js
+++ investments/src/components/InestmentForm/InvestmentForm.js
@@ -1,16 +1,41 @@
 import styles from './InvestmentForm.module.css'
 
 const InvestmentForm = (props) => {
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log('submit');
+    }
+
+    const formResetHandler = (event) => {
+        console.log('reset');
+    }
+
+    const currentSavingsChangeHandler = (event) => {
+
+    }
+
+    const yearlyContributionChangeHandler = (event) => {
+
+    }
+
+    const expectedReturnChangeHandler = (event) => {
+
+    }
+
+    const durationChangeHandler = (event) => {
+
+    }
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={formSubmitHandler}>
             <div className={styles['input-group']}>
                 <p>
                     <label htmlFor="current-savings">Current Savings ($)</label>
-                    <input type="number" id="current-savings"/>
+                    <input type="number" id="current-savings" onChange={currentSavingsChangeHandler}/>
                 </p>
                 <p>
                     <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-                    <input type="number" id="yearly-contribution"/>
+                    <input type="number" id="yearly-contribution" onChange={yearlyContributionChangeHandler}/>
                 </p>
             </div>
             <div className={styles['input-group']}>
@@ -18,18 +43,18 @@ const InvestmentForm = (props) => {
                     <label htmlFor="expected-return">
                         Expected Interest (%, per year)
                     </label>
-                    <input type="number" id="expected-return"/>
+                    <input type="number" id="expected-return" onChange={expectedReturnChangeHandler}/>
                 </p>
                 <p>
                     <label htmlFor="duration">Investment Duration (years)</label>
-                    <input type="number" id="duration"/>
+                    <input type="number" id="duration" onChange={durationChangeHandler}/>
                 </p>
             </div>
             <p className={styles.actions}>
-                <button type="reset" className="buttonAlt">
+                <button type="reset" className={styles.buttonAlt} onClick={formResetHandler}>
                     Reset
                 </button>
-                <button type="submit" className="button">
+                <button type="submit" className={styles.button}>
                     Calculate
                 </button>
             </p>
