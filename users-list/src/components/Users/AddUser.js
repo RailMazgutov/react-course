389877b2d240dd8831cb48859e8ec054e54edd47
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-04-14 21:34:12 +0200

    feat: card component

--- users-list/src/components/Users/AddUser.js
+++ users-list/src/components/Users/AddUser.js
@@ -1,16 +1,20 @@
 import styles from './AddUser.module.css'
+import Card from "../UI/Card";
 
 const AddUser = (props) => {
     const formSubmitHandler = (event) => {
         event.preventDefault();
     };
-    return (<form onSubmit={formSubmitHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text"/>
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"/>
-        <button type="submit">Add User</button>
-    </form>);
+    return (
+        <Card className={styles.input}>
+            <form onSubmit={formSubmitHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"/>
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number"/>
+                <button type="submit">Add User</button>
+            </form>
+        </Card>);
 }
 
 export default AddUser;
