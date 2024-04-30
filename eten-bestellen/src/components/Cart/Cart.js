7e602a516e9c08c3593cc4bd0ac80dcc04b373e3
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-04-30 21:37:20 +0200

    feat eten-bestellen: close cart modal

--- eten-bestellen/src/components/Cart/Cart.js
+++ eten-bestellen/src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import Modal from "../UI/Modal";
 
 const Cart = (props) => {
     const cartItems = [{id: 'c1', name:'Sushi', amount:2, price: 12.99}].map(item => <li>{item.name}</li>);
-    return <Modal>
+    return <Modal onClick={props.onClose}>
         <ul className={styles['cart-items']}>
             {cartItems}
         </ul>
@@ -12,7 +12,7 @@ const Cart = (props) => {
             <span>35.62</span>
         </div>
         <div className={styles.actions}>
-            <button className={styles['button--alt']}>Close</button>
+            <button className={styles['button--alt']} onClick={props.onClose}>Close</button>
             <button className={styles.button}>Order</button>
         </div>
     </Modal>;
