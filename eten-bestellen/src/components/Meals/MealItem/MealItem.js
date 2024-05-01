1413bd450fb867b7be0202e7cec30b762533d869
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-05-01 09:01:36 +0000

    feateten-bestellen: managing cart context

--- eten-bestellen/src/components/Meals/MealItem/MealItem.js
+++ eten-bestellen/src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,21 @@
+import { useContext } from 'react';
 import styles from './MealItem.module.css'
 import MealItemForm from "./MealItemForm";
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
+
+    const cartCtx = useContext(CartContext);
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
+
     return <li className={styles.meal}>
         <div>
             <h3>{props.name}</h3>
@@ -10,7 +23,7 @@ const MealItem = (props) => {
             <div className={styles.price}>{price}</div>
         </div>
         <div>
-            <MealItemForm />
+            <MealItemForm onAddToCart={addToCartHandler}/>
         </div>
     </li>
 };
