bc1b4722c273b434dbe7c008e8c67399d46a60c7
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-05-09 10:31:20 +0200

    feat tic-tac-toe: determine winner

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -3,6 +3,7 @@ import {useState} from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
+import {WINNING_COMBINATIONS} from "./winning-combinations.js";
 
 function deriveActivePlayer(gameTurns) {
     let activePlayer = 'X';
@@ -13,9 +14,31 @@ function deriveActivePlayer(gameTurns) {
     return activePlayer;
 }
 
+const initialGameBoard = [
+    [null,null,null],
+    [null,null,null],
+    [null,null,null],
+];
+
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
     const activePlayer = deriveActivePlayer(gameTurns);
+
+    let gameBoard = initialGameBoard;
+    for (const turn of gameTurns) {
+        const {square, player} = turn;
+        gameBoard[square.row][square.col] = player;
+    }
+
+    let winner;
+    for (const combination of WINNING_COMBINATIONS) {
+        const first = gameBoard[combination[0].row][combination[0].column];
+        const second = gameBoard[combination[1].row][combination[1].column];
+        const third = gameBoard[combination[2].row][combination[2].column];
+        if (first && first === second && first === third) {
+            winner = first;
+        }
+    }
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameTurns(prevTurns => {
@@ -34,7 +57,8 @@ function App() {
                 <Player name="Player 1" symbol="X" isActive={activePlayer==='X'}/>
                 <Player name="Player 2" synbol="O" isActive={activePlayer==='O'}/>
             </ol>
-            <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+            {winner && <p>You won! {winner} </p>}
+            <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
         </div>
         <Log turns={gameTurns}/>
     </main>
