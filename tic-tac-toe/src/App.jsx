ccfb4b9b3ef58e04a931008ecf1a2520fca77c2e
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-05-09 10:09:54 +0200

    feat tic-tac-toe: create game board component

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,12 +1,35 @@
+import {useState} from "react";
+
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
+    const [activePlayer, setActivePlayer] = useState('X');
+    const [gameTurns, setGameTurns] = useState([]);
+    function handleSelectSquare(rowIndex, colIndex) {
+        setActivePlayer(prevPlayer => prevPlayer === 'X'? 'O':'X');
+        setGameTurns(prevTurns => {
+            let currentPlayer = 'X';
+            if (prevTurns.length > 0 && prevTurns[0].player === 'X')
+            {
+                currentPlayer = 'O';
+            }
+
+            return [
+                {
+                    square: {row: rowIndex, col: colIndex},
+                    player: currentPlayer
+                },
+                ...prevTurns];
+        });
+    }
     return <main>
         <div id="game-container">
-            <ol id="players">
-                <Player name="Player 1" symbol="X"/>
-                <Player name="Player 2" synbol="O"/>
+            <ol id="players" className="highlight-player">
+                <Player name="Player 1" symbol="X" isActive={activePlayer==='X'}/>
+                <Player name="Player 2" synbol="O" isActive={activePlayer==='O'}/>
             </ol>
+            <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
         </div>
     </main>
 }
