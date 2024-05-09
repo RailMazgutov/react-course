19ed6052d0def77adb1446d591dc4ca15f1dfdf8
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-05-09 10:13:36 +0200

    feat tic-tac-toe: create log component

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
 
 function App() {
     const [activePlayer, setActivePlayer] = useState('X');
@@ -31,6 +32,7 @@ function App() {
             </ol>
             <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
         </div>
+        <Log turns={gameTurns}/>
     </main>
 }
 
