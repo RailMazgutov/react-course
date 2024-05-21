5d80912607e4b70ac163d63fe9f187280f813f16
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-05-21 10:07:22 +0200

    feat food-order: init meals component

--- food-order/src/App.jsx
+++ food-order/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header.jsx";
+import Meals from "./components/Meals.jsx";
 
 function App() {
-  return (
-    <>
-      <Header />
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <Meals/>
+        </>
+    );
 }
 
 export default App;
