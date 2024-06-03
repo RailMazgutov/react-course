c77392bf774034064d6a97b38ee9c8da6384be73
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-03 09:02:57 +0000

    feat redux-counter: change data in store

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -1,16 +1,29 @@
 import classes from './Counter.module.css';
 
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Counter = () => {
   const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
 
   const toggleCounterHandler = () => {};
+
+  function incrementHandler() {
+    dispatch({type: 'increment'});
+  }
+
+  function decrementHandler() {
+    dispatch({type: 'decrement'});
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>increment</button>
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
