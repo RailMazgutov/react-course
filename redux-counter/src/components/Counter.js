1b5e6ad7d1640f52df085b174650ede4eda41f75
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-04 11:10:18 +0200

    feat redux-counter: multiple fields in redux state

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -1,37 +1,38 @@
 import classes from './Counter.module.css';
 
-import { useSelector, useDispatch } from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 
 const Counter = () => {
-  const counter = useSelector(state => state.counter);
-  const dispatch = useDispatch();
+    const counter = useSelector(state => state.counter);
+    const show = useSelector(state => state.showCounter);
+    const dispatch = useDispatch();
 
-  const toggleCounterHandler = () => {};
+    const toggleCounterHandler = () => {
+        dispatch({type: 'toggle'});
+    };
 
-  function incrementHandler() {
-    dispatch({type: 'increment'});
-  }
+    function incrementHandler() {
+        dispatch({type: 'increment'});
+    }
 
-  const increaseHandler = () => {
-      dispatch({type: 'increase', amount: 5});
-  }
+    const increaseHandler = () => {
+        dispatch({type: 'increase', amount: 5});
+    }
 
-  function decrementHandler() {
-    dispatch({type: 'decrement'});
-  }
+    function decrementHandler() {
+        dispatch({type: 'decrement'});
+    }
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+    return (<main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        {show && <div className={classes.value}>{counter}</div>}
         <div>
             <button onClick={incrementHandler}>increment</button>
             <button onClick={increaseHandler}>increase by 5</button>
             <button onClick={decrementHandler}>decrement</button>
         </div>
         <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+    </main>);
 };
 
 export default Counter;
