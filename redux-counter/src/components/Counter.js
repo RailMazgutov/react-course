020be8681f9f8ce458016537c4990e5435b82e92
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-04 11:01:05 +0200

    feat redux-counter: send payload

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -12,6 +12,10 @@ const Counter = () => {
     dispatch({type: 'increment'});
   }
 
+  const increaseHandler = () => {
+      dispatch({type: 'increase', amount: 5});
+  }
+
   function decrementHandler() {
     dispatch({type: 'decrement'});
   }
@@ -20,11 +24,12 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={incrementHandler}>increment</button>
-        <button onClick={decrementHandler}>decrement</button>
-      </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        <div>
+            <button onClick={incrementHandler}>increment</button>
+            <button onClick={increaseHandler}>increase by 5</button>
+            <button onClick={decrementHandler}>decrement</button>
+        </div>
+        <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
