f4fa2f3197c0e99b9339a89c9ce5aad4a9f45de6
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-07 22:07:01 +0200

    feat router-demo: init router

--- router-demo/src/App.jsx
+++ router-demo/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './App.css'
+import HomePage from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage />}
+]);
 
 function App() {
-
-  return (
-    <div>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
