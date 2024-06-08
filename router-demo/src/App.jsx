c8dfee5925abfbca8b1de2417d1ccdd63c6d7268
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-08 16:22:41 +0200

    feat router-demo: add second route

--- router-demo/src/App.jsx
+++ router-demo/src/App.jsx
@@ -1,9 +1,11 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './App.css'
 import HomePage from "./pages/Home.jsx";
+import Products from "./pages/Products.jsx";
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage />}
+  {path: '/', element: <HomePage />},
+  {path: '/products', element: <Products />}
 ]);
 
 function App() {
