499d730820852dede1a2513ba430ba5f16daa26b
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-10 08:30:48 +0000

    feat router-demo: read path params

--- router-demo/src/App.jsx
+++ router-demo/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import HomePage from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
 import RootLayout from "./pages/RootLayout.jsx";
+import ProductDetail from "./pages/ProfuctDetail.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 
 const router = createBrowserRouter([
@@ -12,7 +13,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {path: '/', element: <HomePage />},
-      {path: '/products', element: <Products />}
+      {path: '/products', element: <Products />},
+      {path: '/products/:productId', element: <ProductDetail />}
     ]
   },
 ]);
