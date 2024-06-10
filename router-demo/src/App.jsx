f7ba68f0fab9cdcfbe0b120a079acb8fcd58529f
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-10 08:13:56 +0000

    feat router-demo: add navigation

--- router-demo/src/App.jsx
+++ router-demo/src/App.jsx
@@ -2,10 +2,17 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './App.css'
 import HomePage from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
+import RootLayout from "./pages/RootLayout.jsx";
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage />},
-  {path: '/products', element: <Products />}
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {path: '/', element: <HomePage />},
+      {path: '/products', element: <Products />}
+    ]
+  },
 ]);
 
 function App() {
