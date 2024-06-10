ef3bcb0adc5f65b1d01c6e5e32b0d1fdc00bcebf
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-10 08:18:05 +0000

    feat router-demo: add error element

--- router-demo/src/App.jsx
+++ router-demo/src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import HomePage from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
 import RootLayout from "./pages/RootLayout.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {path: '/', element: <HomePage />},
       {path: '/products', element: <Products />}
