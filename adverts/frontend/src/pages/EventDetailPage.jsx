ed9830df138581af2b64a472366608f67ae88040
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-12 09:01:30 +0000

    feat advert: complete basic challenge

---
+++ adverts/frontend/src/pages/EventDetailPage.jsx
@@ -0,0 +1,6 @@
+import { useParams } from "react-router-dom";
+
+export default function EventDetailPage() {
+    const params = useParams();
+    return <h1>Event Detail Page id={params.eventId}</h1>;
+}
---
+++ adverts/frontend/src/pages/EventsPage.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+    {id: 'e1', title: "Event 1"},
+    {id: 'e2', title: "Event 2"},
+    {id: 'e3', title: "Event 3"},
+    {id: 'e4', title: "Event 4"},
+]
+
+export default function EventsPage() {
+    return <>
+        <h1>Events Page</h1>
+        <li>
+            {EVENTS.map(event => <ul key={event.id}><Link to={`/events/${event.id}`}>{event.title}</Link></ul>)}
+        </li>
+    </>;
+}
