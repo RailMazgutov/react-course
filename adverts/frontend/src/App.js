448d86b0f3587b89e2b528f29d5734ce06c13057
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-12 09:05:30 +0000

    feat adverts: complete bonus challenge

--- adverts/frontend/src/App.js
+++ adverts/frontend/src/App.js
@@ -6,9 +6,7 @@ import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
 import HomePage from "./pages/HomePage";
-
-
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+import EventsLayout from "./pages/EventsLayout";
 
 const router = createBrowserRouter([
   {
@@ -16,10 +14,16 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       {path: '/', element: <HomePage />},
-      {path: '/events', element: <EventsPage />},
-      {path: '/events/:eventId', element: <EventDetailPage />},
-      {path: '/events/new', element: <NewEventPage />},
-      {path: '/events/:eventId/edit', element: <EditEventPage />},
+      {
+        path: '/events', 
+        element: <EventsLayout />, 
+        children: [
+          {path: '/events', element: <EventsPage />},
+          {path: '/events/:eventId', element: <EventDetailPage />},
+          {path: '/events/new', element: <NewEventPage />},
+          {path: '/events/:eventId/edit', element: <EditEventPage />},
+        ]
+      },
     ]
   },
 ]);
