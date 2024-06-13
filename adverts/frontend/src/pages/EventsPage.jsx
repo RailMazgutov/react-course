2d024fe6dbc3180f629c0eb31c265053a576cf81
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-13 16:01:50 +0000

    feat adverts: fetch data before loading

--- adverts/frontend/src/pages/EventsPage.jsx
+++ adverts/frontend/src/pages/EventsPage.jsx
@@ -1,17 +1,37 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
-const EVENTS = [
-    {id: 'e1', title: "Event 1"},
-    {id: 'e2', title: "Event 2"},
-    {id: 'e3', title: "Event 3"},
-    {id: 'e4', title: "Event 4"},
-]
+import EventsList from '../components/EventsList';
 
-export default function EventsPage() {
-    return <>
-        <h1>Events Page</h1>
-        <li>
-            {EVENTS.map(event => <ul key={event.id}><Link to={event.id}>{event.title}</Link></ul>)}
-        </li>
-    </>;
+function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:8080/events');
+
+      if (!response.ok) {
+        setError('Fetching events failed.');
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+    </>
+  );
 }
+
+export default EventsPage;
