f30ce31267ce5fede36e83557893a50cd5fdfdfc
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-15 21:35:57 +0200

    feat adverts: simple loading state

--- router-demo/src/pages/RootLayout.jsx
+++ router-demo/src/pages/RootLayout.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import {Outlet, useNavigation} from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 
 import classes from './RootLayout.module.css'
 
 export default function RootLayout() {
+    const navigation = useNavigation();
     return <>
         <MainNavigation />
         <main className={classes.content}>
+            {navigation.state === 'loading' && <p>Loading...</p>}
             <Outlet />
         </main>
     </>;
