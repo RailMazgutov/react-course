19d8c07f3f492b2576cb6d083ddb064935580113
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-17 08:42:25 +0000

    feat adverts: use json util to create error object

--- adverts/frontend/src/pages/EventsPage.jsx
+++ adverts/frontend/src/pages/EventsPage.jsx
@@ -1,5 +1,5 @@
 import EventsList from '../components/EventsList';
-import { useLoaderData } from 'react-router-dom';
+import { json, useLoaderData } from 'react-router-dom';
 
 function EventsPage() {
   const response = useLoaderData();
@@ -10,7 +10,7 @@ function EventsPage() {
 export async function loader() {
   const response = await fetch('http://localhost:8080/events');
   if (!response.ok) {
-    throw new Response(JSON.stringify({message: "Could not fetch events"}), {status: 500});
+    return json({message: "Could not fetch events"}, {status: 500});
   } else {
     return response;
   }
