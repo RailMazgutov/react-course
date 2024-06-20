46514218859e98389ff1ad969d15f8fc5c3755f3
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-20 11:01:22 +0200

    feat adverts: use action data to show response from backend

--- adverts/frontend/src/components/EventForm.js
+++ adverts/frontend/src/components/EventForm.js
@@ -1,43 +1,52 @@
-import {Form, useNavigate, useNavigation} from 'react-router-dom';
+import {Form, useActionData, useNavigate, useNavigation} from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
-function EventForm({ method, event }) {
-  const navigate = useNavigate();
-  const navigation = useNavigation();
+function EventForm({method, event}) {
+    const actionData = useActionData();
+    const navigate = useNavigate();
+    const navigation = useNavigation();
 
-  const isSubmitting = navigation.state === 'submitting';
+    const isSubmitting = navigation.state === 'submitting';
 
-  function cancelHandler() {
-    navigate('..');
-  }
+    function cancelHandler() {
+        navigate('..');
+    }
 
-  return (
-    <Form method='post' className={classes.form}>
-      <p>
-        <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" required defaultValue={event ? event.title : ''} />
-      </p>
-      <p>
-        <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" required defaultValue={event ? event.image : ''} />
-      </p>
-      <p>
-        <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" required defaultValue={event ? event.date : ''} />
-      </p>
-      <p>
-        <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : ''}/>
-      </p>
-      <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
-          Cancel
-        </button>
-        <button disabled={isSubmitting}>{isSubmitting ? 'Submitting...' : 'Save'}</button>
-      </div>
-    </Form>
-  );
+    return (
+        <Form method='post' className={classes.form}>
+            { actionData && actionData.errors && (
+                <ul>
+                    {Object.values(actionData.errors).map(err => (
+                        <li key={err}>{err}</li>
+                    ))}
+                </ul>
+            )}
+            <p>
+                <label htmlFor="title">Title</label>
+                <input id="title" type="text" name="title" required defaultValue={event ? event.title : ''}/>
+            </p>
+            <p>
+                <label htmlFor="image">Image</label>
+                <input id="image" type="url" name="image" required defaultValue={event ? event.image : ''}/>
+            </p>
+            <p>
+                <label htmlFor="date">Date</label>
+                <input id="date" type="date" name="date" required defaultValue={event ? event.date : ''}/>
+            </p>
+            <p>
+                <label htmlFor="description">Description</label>
+                <textarea id="description" name="description" rows="5" required
+                          defaultValue={event ? event.description : ''}/>
+            </p>
+            <div className={classes.actions}>
+                <button type="button" onClick={cancelHandler}>
+                    Cancel
+                </button>
+                <button disabled={isSubmitting}>{isSubmitting ? 'Submitting...' : 'Save'}</button>
+            </div>
+        </Form>
+    );
 }
 
 export default EventForm;
