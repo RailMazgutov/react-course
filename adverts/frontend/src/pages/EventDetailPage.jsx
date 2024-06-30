4503a1ed60bfce9f09fae537d6886bb2ce53d027
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-30 21:33:08 +0200

    feat adverts: use defer to call 2 endpoints in a single page

--- adverts/frontend/src/pages/EventDetailPage.jsx
+++ adverts/frontend/src/pages/EventDetailPage.jsx
@@ -1,22 +1,55 @@
-import {json, redirect, useRouteLoaderData} from "react-router-dom";
+import {Await, defer, json, redirect, useRouteLoaderData} from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
+import {Suspense} from "react";
 
 export default function EventDetailPage() {
-    const data = useRouteLoaderData('event-detail');
+    const {event, events} = useRouteLoaderData('event-detail');
     return (
-        <EventItem event={data.event} />
+        <>
+            <Suspense fallback={<p style={{textAlign: 'center'}}>Loading event...</p>}>
+                <Await resolve={event}>
+                    {(event) => <EventItem event={event}/>}
+                </Await>
+            </Suspense>
+            <Suspense fallback={<p style={{textAlign: 'center'}}>Loading events list...</p>}>
+                <Await resolve={events}>
+                    {(events) => <EventsList events={events}/>}
+                </Await>
+            </Suspense>
+        </>
+
     );
 }
 
-export async function loader({request, params}) {
-    const id = params.eventId;
+async function loadEvents() {
+    const response = await fetch('http://localhost:8080/events');
+    if (!response.ok) {
+        return json({message: "Could not fetch events"}, {status: 500});
+    } else {
+        const resData = await response.json();
+        return resData.events;
+    }
+}
+
+async function loadEvent(id) {
     const response = await fetch('http://localhost:8080/events/' + id);
 
     if (!response.ok) {
         throw json({message: "Failed to load the event!"}, {status: 500});
     } else {
-        return response;
+        const resData = await response.json();
+        return resData.event;
     }
+}
+
+export async function loader({request, params}) {
+    const id = params.eventId;
+
+    return defer({
+        events: loadEvents(),
+        event: await loadEvent(id)
+    })
 }
 
 export async function action({request, params}) {
