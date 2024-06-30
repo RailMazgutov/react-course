50b9e2b64a98c4d79cee1fd879179f4f9a7dc7a5
Rail Mazgutov <rail.mazgutov@gmail.com>
2024-06-30 21:24:58 +0200

    feat adverts: use defer to show fallback during loading

--- adverts/frontend/src/pages/EventsPage.jsx
+++ adverts/frontend/src/pages/EventsPage.jsx
@@ -1,19 +1,34 @@
 import EventsList from '../components/EventsList';
-import { json, useLoaderData } from 'react-router-dom';
+import {Await, defer, json, useLoaderData} from 'react-router-dom';
+import {Suspense} from "react";
 
 function EventsPage() {
-  const response = useLoaderData();
-  const events = response.events;
-  return <EventsList events={events} />;
+    const data = useLoaderData();
+
+
+    return (
+        <Suspense fallback={<p style={{textAlign: 'center'}}>Loading...</p>}>
+            <Await resolve={data.events}>
+                {(loadedEvents) => <EventsList events={loadedEvents}/>}
+            </Await>
+        </Suspense>
+    );
 }
 
-export async function loader() {
-  const response = await fetch('http://localhost:8080/events');
-  if (!response.ok) {
-    return json({message: "Could not fetch events"}, {status: 500});
-  } else {
-    return response;
-  }
+async function loadEvents() {
+    const response = await fetch('http://localhost:8080/events');
+    if (!response.ok) {
+        return json({message: "Could not fetch events"}, {status: 500});
+    } else {
+        const resData = await response.json();
+        return resData.events;
+    }
+}
+
+export function loader() {
+    return defer({
+        events: loadEvents()
+    });
 }
 
 export default EventsPage;
